e885e6fcc428a40a906c4c8c6361f9abe4631f34
ikarashi <4655ryo1298@gmail.com>
2019-07-11 17:54:52 +0900

    Initial Commit

---
+++ SlidershowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlidershowApp
+//
+//  Created by 五十嵐諒太 on 2019/07/11.
+//  Copyright © 2019 ikarashiryota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
